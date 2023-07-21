461eb2437a7b2242863f59822f07249164b8771b
parthc2905 <129624511+parthc2905@users.noreply.github.com>
2023-07-21 14:01:20 +0000

    [LeetCode Sync] Runtime - 75 ms (33.43%), Memory - 17.9 MB (94.11%)

--- leetcode/0001-two-sum/solution.py
+++ leetcode/0001-two-sum/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: List[int], target: int ) -> List[int]:
         dict={}
         for i,n in enumerate(nums):
             if n in dict:

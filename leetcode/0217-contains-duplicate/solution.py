97520de01ffccd6b0c464cc8ef8db40c2aaf776b
parthc2905 <129624511+parthc2905@users.noreply.github.com>
2024-10-09 05:51:48 +0000

    [LeetCode Sync] Runtime - 448 ms (5.07%), Memory - 34.6 MB (15.37%)

--- leetcode/0217-contains-duplicate/solution.py
+++ leetcode/0217-contains-duplicate/solution.py
@@ -1,7 +1,21 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        if len(nums)-len(set(nums))>0:
-            return True
-        else:
-            return False
+        # if len(nums)-len(set(nums))>0:
+        #     return True
+        # else:
+        #     return False
+
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i] = 1
+        
+        for i in d.values():
+            if i//2>=1:
+                return True
+        
+        return False
+            

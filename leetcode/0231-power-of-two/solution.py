cdbccf7df11e54320da43398f0bb2a6e22a09383
parthc2905 <129624511+parthc2905@users.noreply.github.com>
2024-12-03 12:19:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.3 MB (82.60%)

---
+++ leetcode/0231-power-of-two/solution.py
@@ -0,0 +1,6 @@
+import math
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0 or n<0:
+            return False
+        return math.floor(math.log2(n)) == math.ceil(math.log2(n))

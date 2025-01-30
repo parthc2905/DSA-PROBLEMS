b86984eb7f99862e9936a46a0ef6c78611afbae1
parthc2905 <129624511+parthc2905@users.noreply.github.com>
2025-01-30 14:29:29 +0000

    [LeetCode Sync] Runtime - 62 ms (62.28%), Memory - 17.8 MB (99.85%)

---
+++ leetcode/3651-transformed-array/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+
+        for i in range(n):
+            
+            result[i] = nums[(i+nums[i])%n]
+        
+        return result

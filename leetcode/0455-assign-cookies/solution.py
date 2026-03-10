31d498b116e3cdefd104e0173fa0d3e705f6742d
parthc2905 <129624511+parthc2905@users.noreply.github.com>
2026-03-10 17:17:46 +0000

    [LeetCode Sync] Runtime - 27 ms (32.75%), Memory - 22.1 MB (66.85%)

---
+++ leetcode/0455-assign-cookies/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        count = 0
+        i,j = 0,0
+        g.sort()
+        s.sort()
+        while i<len(g) and j<len(s):
+            if s[j] >= g[i]:
+                count+=1
+                i+=1
+                j+=1
+            else:
+                j+=1
+        return count
+        
+            
